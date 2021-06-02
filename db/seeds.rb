b8598593eb4e08bdff3493067044c752f662e097
Maurice Welter <mauricewelter7@gmail.com>
2021-06-02 15:56:51 +0200

    All good now

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+5.times do
+  resto = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["Chinese", "Italian", "Japanese", "French", "Belgian"].sample
+  )
+  puts "Created #{resto.name}"
+end
+
+puts "Created #{Restaurant.count} restaurants"
